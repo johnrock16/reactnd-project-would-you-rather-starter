ccf1f39178a6270efdd40c29551d45a51417ca72
joao rocha <joaovrocha16@gmail.com>
2020-11-03 00:39:49 -0300

    submit version

--- src/_DATA.js
+++ src/_DATA.js
@@ -2,7 +2,7 @@ let users = {
   sarahedo: {
     id: 'sarahedo',
     name: 'Sarah Edo',
-    avatarURL:'https://art.ngfiles.com/images/1417000/1417636_everil_sanic.png?f1599509511',
+    avatarURL:'',
     answers: {
       "8xf0y6ziyjabvozdd253nd": 'optionOne',
       "6ni6ok3ym7mf1p33lnez": 'optionTwo',
@@ -14,7 +14,7 @@ let users = {
   tylermcginnis: {
     id: 'tylermcginnis',
     name: 'Tyler McGinnis',
-    avatarURL:'https://pm1.narvii.com/5818/e55ee5fccdd8397e017baaeec1d98d08fa7ac2e9_00.jpg' ,
+    avatarURL:'' ,
     answers: {
       "vthrdm985a262al8qx3do": 'optionOne',
       "xj352vofupe1dqz9emx13r": 'optionTwo',
@@ -24,7 +24,7 @@ let users = {
   johndoe: {
     id: 'johndoe',
     name: 'John Doe',
-    avatarURL:'https://i.redd.it/8t9jolgu1u9z.png' ,
+    avatarURL:'' ,
     answers: {
       "xj352vofupe1dqz9emx13r": 'optionOne',
       "vthrdm985a262al8qx3do": 'optionTwo',
