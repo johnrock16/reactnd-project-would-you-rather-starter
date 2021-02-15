b73e381b70c2364ce42ba11eee7947a2528eb24a
johnrock16 <joaovrocha16@gmail.com>
2021-02-14 22:04:30 -0300

    update readme

--- src/_DATA.js
+++ src/_DATA.js
@@ -2,7 +2,7 @@ let users = {
   sarahedo: {
     id: 'sarahedo',
     name: 'Sarah Edo',
-    avatarURL:'',
+    avatarURL:'https://images.pexels.com/photos/3658781/pexels-photo-3658781.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
     answers: {
       "8xf0y6ziyjabvozdd253nd": 'optionOne',
       "6ni6ok3ym7mf1p33lnez": 'optionTwo',
@@ -14,7 +14,7 @@ let users = {
   tylermcginnis: {
     id: 'tylermcginnis',
     name: 'Tyler McGinnis',
-    avatarURL:'' ,
+    avatarURL:'https://images.pexels.com/photos/433142/pexels-photo-433142.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' ,
     answers: {
       "vthrdm985a262al8qx3do": 'optionOne',
       "xj352vofupe1dqz9emx13r": 'optionTwo',
@@ -24,7 +24,7 @@ let users = {
   johndoe: {
     id: 'johndoe',
     name: 'John Doe',
-    avatarURL:'' ,
+    avatarURL:'https://images.pexels.com/photos/6326348/pexels-photo-6326348.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' ,
     answers: {
       "xj352vofupe1dqz9emx13r": 'optionOne',
       "vthrdm985a262al8qx3do": 'optionTwo',
